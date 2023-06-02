92eaa02bb3cb641c580e8626e935da8a28366de9
Jedi Burrell <jediburrell@gmail.com>
2023-06-02 11:06:13 -0400

    Add documentation to GenericTransformUpdateDetails

--- lib/infinity_view.dart
+++ lib/infinity_view.dart
@@ -284,13 +284,59 @@ class GenericTransformStartDetails {
   }
 }
 
+/// [GenericTransformUpdateDetails] genericizes all used input events into a
+/// single class. This allows us to use the same logic for all input events.
+///
+/// Not all properties are used for all input events. For example, the
+/// [PointerMoveEvent] does not have a [scale] property, so it will always
+/// be 1.0.
+///
+/// You can use [kind] to determine the input type.
 class GenericTransformUpdateDetails {
+  /// The global focal point of the pointers in contact with the screen.
+  ///
+  /// Typically this is where the touch event is happening, though if the user
+  /// is performing a pinch or zoom gesture involving multiple fingers this
+  /// is the center of those points.
+  ///
+  /// Supports mouse, trackpad, and touch events.
   final Offset focalPoint;
+
+  /// The local focal point of the pointers in contact with the screen.
+  ///
+  /// Typically this is where the touch event is happening, though if the user
+  /// is performing a pinch or zoom gesture involving multiple fingers this
+  /// is the center of those points.
+  ///
+  /// Supports mouse, trackpad, and touch events.
   final Offset localFocalPoint;
+
+  /// This is the scale factor for the scale event.
+  ///
+  /// Supports mouse, trackpad, and touch events.
+  /// Defaults to 1.0 when no scaling is happening.
   final double scale;
+
+  /// This is the rotation factor for the rotation event.
+  ///
+  /// Supports trackpad and touch events.
+  /// Defaults to 0.0 when no rotation is happening.
   final double rotation;
+
+  /// The kind of input device from which the update originated.
   final PointerDeviceKind kind;
+
+  /// The buttons that were pressed when the pointer event occurred.
+  ///
+  /// This is a nullable value because not all input devices have buttons.
+  /// You should check the input device's [kind] before using this value.
+  ///
+  /// Supports mouse and trackpad events.
   final int? buttons;
+
+  /// The number of pointers involved in the event.
+  ///
+  /// Supports mouse, trackpad, and touch events.
   final int pointerCount;
 
   GenericTransformUpdateDetails({
